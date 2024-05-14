c3142d9666395fd38adfcebd59ba486bd4540045
YosriElhadj <yosri.elhadj@esprit.tn>
2024-05-14 05:59:24 +0100

    another proprity added to Admin class

--- src/Entity/Admin.php
+++ src/Entity/Admin.php
@@ -8,18 +8,13 @@ use Doctrine\ORM\Mapping as ORM;
 #[ORM\Entity(repositoryClass: AdminRepository::class)]
 class Admin extends AbstractUtilisateur
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
+    // #[ORM\Id]
+    // #[ORM\GeneratedValue]
+    // #[ORM\Column]
+    // private ?int $id = null;
 
     #[ORM\Column]
     private ?float $salaire = null;
-
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
 
     public function getSalaire(): ?float
     {
